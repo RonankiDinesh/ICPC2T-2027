4e62605e5b72a1873d4b649eacea817ea841e2d5
Dinesh <dineshronanki777@gmail.com>
2026-04-07 14:37:08 +0530

    Done some changes

--- src/views/Advisory_Committee.vue
+++ src/views/Advisory_Committee.vue
@@ -1,27 +1,49 @@
 <template>
   <div class="col-xl-8 col-md-12">
-    <!-- Post -->
     <div class="row mt-2 mb-5 pb-3 mx-2">
-      <!-- Card -->
       <div class="card card-body mb-5">
+
+        <!-- Main Heading -->
         <h2 class="font-weight-bold mt-3">
-          <strong>Advisory Board Members(Tentative)</strong>
+          <strong>Advisory Board Members (Tentative)</strong>
         </h2>
         <hr class="red title-hr" />
+
+        <!-- International Advisory Board -->
+        <h3 class="mt-4"><strong>International Advisory Board Members</strong></h3>
+        <hr class="red title-hr" />
         <p class="article">
-          <span>Dr. Geetam Tomar, Rajkiya Engg. College, Sonbhadra, UP</span> <br />
-          <span>Shri. M K Badapanda, RRCAT, Indore</span> <br />
-          <span>Prof. Bijay Ketan Panigrahy, IIT Delhi </span> <br />
-          <span>Prof. Sukumar Mishra, IIT Dhanbad </span> <br />
-          <span>Prof. Ashok Kumar Pradhan, IIT Kharagpur </span> <br />
-          <span>Prof. Bidyadhar Subudhi, NIT Warangal </span> <br />
-          <span>Prof. Ramesh Bansal, University of Pretoria, South Africa</span> <br />
-          <span>Prof. Sanjib Panda, NTU Singapore </span> <br />
-          <span>Prof. Frede Blaabjerg, Aalborg University, Denmark</span> <br />
-          <span>Prof. Jih-Sheng (Jason) Lai, Virginia Tech, USA </span> <br />
+          <span>Prof. Bikash Pal, Imperial College London</span><br />
+          <span>Prof. Ayman El-Refaie, President IAS IEEE</span><br />
+          <span>Prof. Sanjib Panda, National University of Singapore, Singapore</span><br />
+          <span>Prof. Frede Blaabjerg, Aalborg University, Denmark</span><br />
+          <span>Prof. Jih-Sheng (Jason) Lai, Virginia Tech, USA</span><br />
+          <span>Prof. Akshay Rathore, National University of Singapore, Singapore</span><br />
+          <span>Prof. Ramesh Bansal, University of Pretoria, South Africa</span><br />
+          <span>Prof. Vinod Khadkikar, Khalifa University of Science & Technology, UAE</span><br />
+          <span>Prof. Kashem Muttaqi, University of Wollongong, Australia</span><br />
+          <span>Prof. Biplab Sikdar, National University of Singapore, Singapore</span><br />
+          <span>Dr. Brij N. Singh, John Deere Inc, USA</span><br />
         </p>
+
+        <!-- National Advisory Board -->
+        <h3 class="mt-4"><strong>National Advisory Board Members</strong></h3>
+        <hr class="red title-hr" />
+        <p class="article">
+          <span>Prof. Bijay Ketan Panigrahy, IIT Delhi</span><br />
+          <span>Prof. Sukumar Mishra, IIT(ISM) Dhanbad</span><br />
+          <span>Prof. Bidyadhar Subudhi, NIT Warangal</span><br />
+          <span>Prof. Manisha Dubey, MANIT Bhopal</span><br />
+          <span>Prof. Rajesh Kumar Nema, MANIT Bhopal</span><br />
+          <span>Prof. Savita Nema, MANIT Bhopal</span><br />
+          <span>Prof. Jyoti Singhi, MANIT Bhopal</span><br />
+          <span>Prof. Ram Bilas Pachori, IIT Indore</span><br />
+          <span>Prof. Kiran Kumar Pattanaik, IITM Gwalior</span><br />
+          <span>Prof. Durgesh Kumar Mishra, SAIT Indore</span><br />
+          <span>Prof. Sanjeev Singh, MANIT Bhopal</span><br />
+        </p>
+
       </div>
-      <!-- entry-content clearfix-->
 
       <div id="comments" class="comments-area"></div>
     </div>
@@ -33,3 +55,18 @@ export default {
   name: "Advisory_Committee",
 };
 </script>
+
+<style scoped>
+.article {
+  line-height: 1.8;
+  font-family: sans-serif;
+}
+
+h3 {
+  font-weight: 600;
+}
+
+.title-hr {
+  margin-bottom: 15px;
+}
+</style>
